ac0c1e90217a01b8479fdb3636da9b9e1eeb09d8
Chibuzo <chibuzo.henry@gmail.com>
2024-02-14 10:35:14 +0100

    temp modification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,88 +14,88 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+    constructor(private readonly authService: AuthService) { }
 
-        // view all admin
-        @Get()
-        @UseGuards(AuthGuard())
-        async getAllUsers(){
-            return await this.authService.getAllUser()
-        }
+    // view all admin
+    @Get()
+    @UseGuards(AuthGuard())
+    async getAllUsers() {
+        return await this.authService.getAllUser()
+    }
 
-        
-        // new admin signup
-        @Post('/')
-        @UseGuards(AuthGuard(), RolesGuard)
-        @Roles('SUPER_ADMIN') //You can pass multiple roles
-        @UsePipes(new userValidation(UserSchema))
-        async signup(
-            @Body()
-            signUpDto: SignUpDto,
-            @CurrentUser() user:User
-            ): Promise<{token: string}>{
-                const newUser = this.authService.newUser(signUpDto, user);
-                return newUser;
-        }
 
-        // Admin login
-        @Post('/login')
-        async login(
-            @Body(new userValidation(LoginSchema) ) 
-            loginDto:LoginDto
-        ):Promise<{token: string}>{
-            return this.authService.login(loginDto)
-        }
+    // new admin signup
+    @Post('/')
+    // @UseGuards(AuthGuard(), RolesGuard)
+    // @Roles('SUPER_ADMIN') //You can pass multiple roles
+    @UsePipes(new userValidation(UserSchema))
+    async signup(
+        @Body()
+        signUpDto: SignUpDto,
+        @CurrentUser() user: User
+    ): Promise<User> {
+        const newUser = this.authService.newUser(signUpDto, user);
+        return newUser;
+    }
 
-        // Edit user profile
-        @Patch(':id')
-        @UseGuards(AuthGuard(), RolesGuard)
-        async updateUser(
-            @Body()
-            updateUserDto: UpdateUserDto,
-            @Param('id')id: string,
-            @CurrentUser() user: User,
-        ){
-            return this.authService.editProfile(parseInt(id), user, updateUserDto);
-        }
+    // Admin login
+    @Post('/login')
+    async login(
+        @Body(new userValidation(LoginSchema))
+        loginDto: LoginDto
+    ): Promise<{ token: string }> {
+        return this.authService.login(loginDto)
+    }
 
-        // View Trash
-        @Get('trash')
-        @UseGuards(AuthGuard())
-        async viewtrash(){
-            return await this.authService.viewTrash()
-        }
+    // Edit user profile
+    @Patch(':id')
+    @UseGuards(AuthGuard(), RolesGuard)
+    async updateUser(
+        @Body()
+        updateUserDto: UpdateUserDto,
+        @Param('id') id: string,
+        @CurrentUser() user: User,
+    ) {
+        return this.authService.editProfile(parseInt(id), user, updateUserDto);
+    }
 
-        //Soft delete user
-        @Delete('trash/:id')
-        @UseGuards(AuthGuard(), RolesGuard)
-        @Roles('ADMIN', 'SUPER_ADMIN') //You can pass multiple roles
-        async trash(
-            @Param('id') id: string,
-            @CurrentUser() user:User
-        ){
-            return this.authService.trash(parseInt(id), user);
-        }
+    // View Trash
+    @Get('trash')
+    @UseGuards(AuthGuard())
+    async viewtrash() {
+        return await this.authService.viewTrash()
+    }
 
-        // Restore a soft deleted Admin
-        @Put('restore/:id')
-        @UseGuards(AuthGuard(), RolesGuard)
-        @Roles('SUPER_ADMIN')
-        async restore(
-            @Param('id') id: string,
-            @CurrentUser() user:User
-        ){
-            return this.authService.restore(parseInt(id), user);
-        }
+    //Soft delete user
+    @Delete('trash/:id')
+    @UseGuards(AuthGuard(), RolesGuard)
+    @Roles('ADMIN', 'SUPER_ADMIN') //You can pass multiple roles
+    async trash(
+        @Param('id') id: string,
+        @CurrentUser() user: User
+    ) {
+        return this.authService.trash(parseInt(id), user);
+    }
 
-        // Permanant Delete
-         @Delete('delete/:id')
-         @UseGuards(AuthGuard(), RolesGuard)
-         @Roles('SUPER_ADMIN') //You can pass multiple roles
-         async delete(
-             @Param('id') id: string,
-             @CurrentUser() user:User
-         ){
-             return this.authService.delete(parseInt(id), user);
-         }
+    // Restore a soft deleted Admin
+    @Put('restore/:id')
+    @UseGuards(AuthGuard(), RolesGuard)
+    @Roles('SUPER_ADMIN')
+    async restore(
+        @Param('id') id: string,
+        @CurrentUser() user: User
+    ) {
+        return this.authService.restore(parseInt(id), user);
+    }
+
+    // Permanant Delete
+    @Delete('delete/:id')
+    @UseGuards(AuthGuard(), RolesGuard)
+    @Roles('SUPER_ADMIN') //You can pass multiple roles
+    async delete(
+        @Param('id') id: string,
+        @CurrentUser() user: User
+    ) {
+        return this.authService.delete(parseInt(id), user);
+    }
 }
